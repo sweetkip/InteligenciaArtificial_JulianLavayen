88096aa1d42262c7f31c650d99d3f6badd78bfd8
sweetkip <julilava10@gmail.com>
2026-06-04 03:21:07 -0300

    Update SceneController.cs

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -3,8 +3,60 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public static SceneController instance {  get; private set; }
+
+    [Header("UI Objects")]
+    [SerializeField] private GameObject victory;
+    [SerializeField] private GameObject defeat;
+
+    private int totalPokemon = 3;
+    private int caughtPokemon = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }    
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    private void Start()
+    {
+        if (victory != null) victory.SetActive(false);
+        if (defeat != null) defeat.SetActive(false);
+    }
+
     public void LoadSceneByName(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void TriggerDefeat()
+    {
+        if (defeat != null)
+        {
+            defeat.SetActive(true);
+            UnlockCursor();
+        }    
+    }
+
+    public void TriggerVictory()
+    {
+        if (victory != null)
+        {
+            victory.SetActive(true);
+            UnlockCursor();
+        }
+    }
+
+    private void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
